c0044a5785ee62d7d87b70f2ac1324a4dffe51a8
Aiswarya1810 <s4070432@student.rmit.edu.au>
2026-03-22 12:07:47 +1100

    create page

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import { Provider } from "@/components/ui/provider";
+import React from "react";
+import App from './App.jsx';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+
+);
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,61 @@
+import { useColorModeValue } from '@/components/ui/color-mode';
+import { Box, Button, Container, VStack, Heading } from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+function CreatePage() {
+    const[newProduct, setNewProduct]= useState({
+        name:"",
+        price:"",
+        image:"",
+    });
+const handleAddProduct =() => {
+    console.log(newProduct)};
+
+
+  return <Container maxW={"container.sm"}>
+    <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+            Create new Product
+        </Heading>
+
+        <Box w={"full"} bg={useColorModeValue("white","gray.800")}
+         p={6} rounded={"lg"} shadow={"md"}>
+
+        <VStack spacing={4}>
+            <input 
+            placeholder='Product Name'
+            name='name'
+            value={newProduct.name}
+            onChange={(e) =>setNewProduct({ ...newProduct,name: e.target.value})
+            }/>
+
+            <input 
+            placeholder='Price'
+            name='Price'
+            type='number'
+            value={newProduct.price}
+            onChange={(e) =>setNewProduct({ ...newProduct,price: e.target.value})
+            }/>
+
+            <input 
+            placeholder='Image url'
+            name='image'
+            value={newProduct.image}
+            onChange={(e) =>setNewProduct({ ...newProduct,image: e.target.value})
+            }/>
+
+
+<Button colorScheme='blue' onClick={handleAddProduct} w='full'>
+    Add Product
+</Button>
+
+        </VStack>
+         </Box>
+    </VStack>
+
+
+  </Container>
+  
+};
+
+export default CreatePage;
